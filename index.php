ef18c74bb806cb83a5b69913dcc1b1db52e0367a
ChrisGreta <165997041+HalfwayConcept@users.noreply.github.com>
2025-03-11 17:07:14 +0100

    Première étape du modèle MVC

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "Hello World";
+//paramètre de routage
+$route = isset($_GET['route']) ? $_GET['route'] : 'home';
+
+switch ($route) {
+    case 'home':
+        require_once('controllers/home.controller.php');
+        index();
+        break;
+    default:
+        echo "Page non trouvé";
+        break;
+}
